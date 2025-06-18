c6bd8e149560a9ef1d4f455f106391b150f358bd
Shreyas <shreyastungar762@gmail.com>
2025-06-19 02:39:20 +0530

    small change

--- Backend/index.js
+++ Backend/index.js
@@ -5,28 +5,12 @@ const cors = require("cors");
 const port = process.env.port || 8000;
 const mongoose = require("./config/mongoos");
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://todo-list-lyart-chi.vercel.app",
-];
-
-// Updated CORS configuration
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.options("*", cors());
+app.use(cors({
+  origin :"*",
+  methods :["GET","POST","PUT","DELETE"],
+  allowedHeaders :["Content-Type","Authorization"],
+  credentials :true,
+}));
 
 //use json format to read data
 app.use(express.json());
--- Frontend/src/config/api.js
+++ Frontend/src/config/api.js
@@ -1,2 +1,2 @@
- export const API_BASE_URL = "https://your-production-url.com/api";
+ export const API_BASE_URL = "https://todo-list-backend-three.vercel.app/api";
 // export const API_BASE_URL = "http://localhost:8000/api";
